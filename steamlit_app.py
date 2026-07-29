d5647d4357c9307385fdf301d314dde7e5fd1670
santoshAkula <40515634+santoshAkula@users.noreply.github.com>
2026-07-29 14:37:06 +0530

    steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -49,7 +49,8 @@ if time_to_insert:
 
 #smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)") 
 smoothiefroot_response = requests.get(    "https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), user_contrainer_width=true )
 
     
     
