53a28f922784f7ff31c6bee47e1ff5683e2c0b61
santoshAkula <40515634+santoshAkula@users.noreply.github.com>
2026-07-29 14:44:00 +0530

    steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -34,7 +34,8 @@ ingredients_list = st.multiselect('Choose upto 5 Incredents', my_dataframe, max_
 ingredients_string = ''
 for fruit_choosen in ingredients_list:
     ingredients_string += fruit_choosen + ' '
-
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True )
 st.write(ingredients_string)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -48,9 +49,9 @@ if time_to_insert:
     st.success('Your Smoothie is ordered!', icon="✅")
 
 #smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)") 
-smoothiefroot_response = requests.get(    "https://my.smoothiefroot.com/api/fruit/watermelon")
+
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True )
+
 
     
     
